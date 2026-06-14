278b265dafcaf9c12ad9cbd26b16bbcd0326a891
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-14 00:22:45 -0300

    feat: add auth.ts to reset password

---
+++ src/services/auth.ts
@@ -0,0 +1,30 @@
+import { api } from "./api";
+
+export interface ForgotPasswordPayload {
+    email: string
+}
+
+export interface ResetPasswordPayload {
+  token: string
+  newPassword: string
+}
+
+export interface MessageResponse {
+    message: string
+}
+
+export async function forgotPassword(
+    payload: ForgotPasswordPayload,
+ ): Promise<MessageResponse>{
+    const { data } = await api.post<MessageResponse>('/forgot-password', payload)
+    return data
+}
+
+export async function resetPassword(
+    payload: ResetPasswordPayload,
+): Promise<MessageResponse> {
+    const { data } = await api.post<MessageResponse>('/reset-password', payload )
+    return data
+}
+
+
