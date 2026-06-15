e28a818cb9e4d33e59919ee39d23d3419f1778a3
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-15 20:54:08 -0300

    feat: add icon visibility password

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,20 @@
 import { api } from "@/services/api";
 import { Link, useNavigate } from "react-router-dom";
 import React, { useState } from "react";
-import AlertError from '@/components/ui/Alert-error'
-import AlertWarning from "@/components/ui/Alet-warning";
+import AlertWarning from "@/@/components/ui/alert-warning";
+import AlertError from "@/@/components/ui/alert-error";
 import Cookies from 'js-cookie'
-import { useAuth } from "./AuthLogin"; 
+import { useAuth } from "./AuthLogin";
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
 
 export default function Login() {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const navigate = useNavigate()
     const { login } = useAuth()
-    const [ alertType, setAlertType ] = useState<'error' | 'warning' | null>(null)
+    const [showPassword, setShowPassword] = useState(false)
+    const [alertType, setAlertType] = useState<'error' | 'warning' | null>(null)
 
     const handleSubmit = async (e: React.SubmitEvent<HTMLFormElement>) => {
         e.preventDefault()
@@ -24,7 +27,7 @@ export default function Login() {
             login(data.token, {
                 name: data.user.name,
                 picture: data.user.picture,
-                email: data.user.picture,
+                email: data.user.email,
                 phone: data.user.phone
             })
 
@@ -44,53 +47,62 @@ export default function Login() {
     }
     return (
         <div className="">
-            
+
             <section className="border-be-blue-950 bg-gray-100 rounded shadow flex flex-col w-80 m-auto justify-items-center justify-center px-2 py-4 mt-20">
-                <h2 className="mt-1 font-bold text-lg justify-center sm:text-2xl flex justify-items-center py-2"> Jobflow AI</h2>
+                <h2 className="mt-1 font-bold text-lg justify-center sm:text-2xl flex justify-items-center text-fuchsia-700 py-2"> Jobflow AI</h2>
 
                 <form className="px-1"
-                 onSubmit={handleSubmit}>
-                    {alertType === 'error' && <AlertError/>}
-                    {alertType === 'warning' && <AlertWarning/>}
-                <div className="flex flex-col">
-                    <label className="py-2" htmlFor="email">Email </label>
-                    
-                    <input
-                        className="px-2 py-2 outline-none bg-gray-200 rounded"
-                        type="email"
-                        name="email"
-                        id="email"
-                        value={email}
-                        placeholder="Digite seu email*"
-                        required
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
+                    onSubmit={handleSubmit}>
+                    {alertType === 'error' && <AlertError />}
+                    {alertType === 'warning' && <AlertWarning />}
+                    <div className="flex flex-col">
+                        <label className="py-2" htmlFor="email">Email </label>
 
-                <div className="flex flex-col">
-                    <label className="py-2" htmlFor="password">Senha</label>
-                    <input
-                        className="px-2 py-2 outline-none bg-gray-200 rounded"
-                        type="password"
-                        name="password"
-                        id="password"
-                        value={password}
-                        placeholder="Digite sua senha*"
-                        required
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
+                        <input
+                            className="px-2 py-2 outline-none bg-gray-200 rounded"
+                            type="email"
+                            name="email"
+                            id="email"
+                            value={email}
+                            placeholder="Digite seu email*"
+                            required
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
 
-                <div className="justify-center justify-items-center">
-                    <button className="text-amber-50 font-semibold bg-blue-500 w-full mt-3 rounded px-2 py-2" type="submit">Acessar</button>
-                </div>
+                    <div className="flex flex-col relative">
+                        <label className="py-2" htmlFor="password">Senha</label>
+                        <input
+                            className="px-2 py-2 outline-none bg-gray-200 rounded"
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            id="password"
+                            value={password}
+                            placeholder="Digite sua senha*"
+                            required
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                        <button className=" absolute right-2 mt-12 text-gray-500 "
+                            onClick={() => setShowPassword(prev => !prev)}
+                        >
+                            {showPassword ? (
+                                <VisibilityIcon fontSize="small" />
+                            ) : (
+                                <VisibilityOffIcon fontSize="small" />
+                            )}
+                        </button>
+                    </div>
 
-                <div className="flex justify-between mt-2 text-blue-600">
-                    <Link to='/CadastroUsuario'>Ja tem cadastro?</Link>
-                    <Link to='/esqucer-senha'>Esqueceu a senha?</Link>
-                </div>
+                    <div className="justify-center justify-items-center">
+                        <button className="text-amber-50 font-semibold bg-foreground w-full mt-3 rounded px-2 py-2" type="submit">Acessar</button>
+                    </div>
 
-            </form>
+                    <div className="flex justify-between mt-2 text-blue-600">
+                        <Link to='/CadastroUsuario'>Ja tem cadastro?</Link>
+                        <Link to='/esqucer-senha'>Esqueceu a senha?</Link>
+                    </div>
+
+                </form>
 
             </section>
         </div>
