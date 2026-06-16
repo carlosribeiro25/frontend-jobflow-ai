29a99821b3c73d90277a1b94e9f885ba003f884a
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-16 02:28:29 -0300

    feat: add Sidebar.tsx

---
+++ src/@/components/layout/Sidebar.tsx
@@ -0,0 +1,32 @@
+import { Sidebar, SidebarContent, SidebarFooter, SidebarGroup, SidebarGroupLabel, SidebarHeader, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/@/components/ui/sidebar"
+
+export default function AppSidebar() {
+
+    return (
+            <Sidebar collapsible="icon">
+                <SidebarHeader>
+                    <SidebarMenu>
+                        <SidebarMenuItem>
+                           <SidebarMenuButton>
+                           
+                            </SidebarMenuButton> 
+                        </SidebarMenuItem>
+                    </SidebarMenu>
+                </SidebarHeader>
+
+                <SidebarContent>
+                    <SidebarGroup>
+                        <SidebarGroupLabel>
+                            
+                        </SidebarGroupLabel>
+
+
+                    </SidebarGroup>
+                </SidebarContent>
+                <SidebarFooter>
+                    
+                </SidebarFooter>
+            </Sidebar>
+
+    )
+}
