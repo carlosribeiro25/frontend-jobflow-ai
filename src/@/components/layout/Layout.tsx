62c9bbde3be96253d19d9b81d0b3dc5e88822904
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-16 10:34:49 -0300

    feat: add Layout.tsx component

--- src/@/components/layout/Layout.tsx
+++ src/@/components/layout/Layout.tsx
@@ -1,14 +1,12 @@
 import { SidebarProvider, SidebarInset } from "../ui/sidebar";
 import AppSidebar from "./Sidebar";
-import { AppHeader } from "./Header";
-
 
 export default function AppLayout() {
     return (
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <AppHeader />
+                
                 <main className="flex-1">
                     conteudo
                 </main>
