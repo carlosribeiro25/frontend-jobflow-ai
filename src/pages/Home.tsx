c911b95d9604be24af46fedb0f71b4d77202f472
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-16 10:37:28 -0300

    feat: add Avatar in header home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,17 @@
+import { AvatarMenu } from "@/@/components/layout/AvatarDropdown";
 import AppSidebar from "@/@/components/layout/Sidebar";
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/@/components/ui/sidebar";
 
-export default function HomePage() {
 
+export default function HomePage() {
     return (
         <SidebarProvider>
             <AppSidebar/>
+            
             <SidebarInset>
-                <header className="flex h-16 items-center border-b px-4">
+                <header className="flex justify-between bg-amber-600 h-16 items-center border-b px-4">
                     <SidebarTrigger/>
+                    <AvatarMenu/>
                 </header>
 
                 <main className="p-6">
