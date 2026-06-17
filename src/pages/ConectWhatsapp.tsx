5db444504fa1cd38ce09e1e782c0e95441ef7346
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-17 15:08:12 -0300

    feat: add button conect whatsapp

---
+++ src/pages/ConectWhatsapp.tsx
@@ -0,0 +1,9 @@
+import { WhatsappConnect } from "@/@/components/layout/WhatsappConnect";
+
+export default function ConectionWhatsapp() {
+    return (
+        <div>
+           <WhatsappConnect/> 
+        </div>
+    )
+}
