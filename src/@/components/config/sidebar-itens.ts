2ccb4ed65171f5ca17e8cecf14348d07cdb1e7f6
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-29 07:52:44 -0300

    feat: add navegation by items with sidebarItens

---
+++ src/@/components/config/sidebar-itens.ts
@@ -0,0 +1,21 @@
+import HomeFilledIcon from '@mui/icons-material/HomeFilled'
+import EngineeringIcon from '@mui/icons-material/Engineering'
+import SettingsIcon from '@mui/icons-material/Settings'
+
+export const sidebarItens = [
+  {
+    title: 'Home',
+    path: '/',
+    icon: HomeFilledIcon,
+  },
+  {
+    title: 'Buscar por filtro',
+    path: '/filtros',
+    icon: EngineeringIcon,
+  },
+  {
+    title: 'Configurações',
+    path: '/settings',
+    icon: SettingsIcon,
+  },
+]
