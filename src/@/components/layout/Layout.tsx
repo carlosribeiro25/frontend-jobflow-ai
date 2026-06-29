9670eb6abae0696f5c55be0c40708fb991774e8c
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-29 07:49:34 -0300

    feat: add Layout structure

--- src/@/components/layout/Layout.tsx
+++ src/@/components/layout/Layout.tsx
@@ -1,12 +1,19 @@
+import { Outlet } from 'react-router-dom'
 import { SidebarProvider, SidebarInset } from '../ui/sidebar'
+import { AppHeader } from './Header'
 import AppSidebar from './Sidebar'
 
 export default function AppLayout() {
   return (
     <SidebarProvider>
       <AppSidebar />
+
       <SidebarInset>
-        <main className="flex-1">conteudo</main>
+        <AppHeader />
+
+        <main className="flex-1 p-6">
+          <Outlet />
+        </main>
       </SidebarInset>
     </SidebarProvider>
   )
