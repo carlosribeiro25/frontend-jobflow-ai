1f81f26fe8f312696fb468e54cf27859a2ff30ba
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-29 19:57:29 -0300

    feat: add searchVagas function for job search API integration

---
+++ src/@/components/Vagas/Vagas-Search.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { api } from "@/services/api";
+import type { SearchVagasResponse } from "@/types/vaga";
+
+interface SearchVagasParam {
+    q: string
+    page?: number
+    limit?: number
+}
+
+export async function searchVagas({
+    q,
+    page = 1,
+    limit = 10
+} : SearchVagasParam) : Promise<SearchVagasResponse> {
+    try {
+        const response = await api.get<SearchVagasResponse>('/search', {
+            params: {q, page, limit},
+        })
+
+        return response.data
+
+    } catch (error) {
+        if (axios.isAxiosError(error) && error.response?.status === 404) {
+            return { vagas: [], total: 0}
+        }
+
+        throw error
+    }
+}
