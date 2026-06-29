5c522c352151ddd6276d007789e4843f109d7595
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-06-29 19:49:28 -0300

    feat: update AppHeader layout and integrate SearchVagas component

--- src/@/components/layout/Header.tsx
+++ src/@/components/layout/Header.tsx
@@ -1,21 +1,22 @@
 import { ModeToggle } from '../theme/mode-toggle'
 import { SidebarTrigger } from '../ui/sidebar'
+import { SearchVagas } from '../Vagas/SearchVagas'
 import { AvatarMenu } from './AvatarDropdown'
-import WhatsappAction from './WhatsappAction'
 
 export function AppHeader() {
   return (
     <div>
-      <header className="flex w-full bg-amber-400  justify-between h-16 items-center border-b px-4">
+      <header className="flex w-full justify-between h-16 items-center border-b px-4">
         <div className="flex items-center gap-2">
           <SidebarTrigger />
           <h1 className="font-extrabold lg:text-2xl md:text-2xl text-fuchsia-700 ">Jobflow AI</h1>
         </div>
 
-        <div className="flex items-center gap-4">
+        <div className="flex justify-between  items-center gap-4">
+            <SearchVagas/>
           <ModeToggle />
           <div className="hidden md:block">
-            <WhatsappAction />
+            
           </div>
           <AvatarMenu />
         </div>
