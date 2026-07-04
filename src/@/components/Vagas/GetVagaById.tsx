2a2bdb5b0c47287b932dd6eb93df356da4ff7b6e
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-07-04 04:22:54 -0300

    feat: add typing anotation for getVaga function

--- src/@/components/Vagas/GetVagaById.tsx
+++ src/@/components/Vagas/GetVagaById.tsx
@@ -1,7 +1,8 @@
 import { api } from '@/services/api'
+import type { Vaga } from '@/types/vaga'
 
-export async function getVagasId(id: number) {
-  const response = await api.get(`/vagas/${id}`)
+export async function getVagasId(id: number): Promise<Vaga> {
+  const response = await api.get<Vaga>(`/vagas/${id}`)
 
   return response.data
 }
