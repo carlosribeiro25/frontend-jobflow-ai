3949bb0af1a8afb30faf9defc6918f188fe3b453
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-07-06 02:33:54 -0300

    feat: create mobile navigation footer with dynamic items

---
+++ src/@/components/config/footer-mobile.ts
@@ -0,0 +1,27 @@
+import HomeFilledIcon from '@mui/icons-material/HomeFilled'
+import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest'
+import WhatsAppIcon from '@mui/icons-material/WhatsApp'
+import FilterListAltIcon from '@mui/icons-material/FilterListAlt'
+
+export const itensNavMobile = [
+  {
+    label: 'Início',
+    path: '/',
+    icon: HomeFilledIcon,
+  },
+  {
+    label: 'Configurações',
+    path: '/settings',
+    icon: SettingsSuggestIcon,
+  },
+  {
+    label: 'Filtros',
+    path: '/filtros',
+    icon: FilterListAltIcon,
+  },
+  {
+    label: 'WhatsApp',
+    path: '/conectWhatsapp',
+    icon: WhatsAppIcon,
+  },
+]
