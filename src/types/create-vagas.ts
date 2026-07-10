541cc8601df199a109a585d74be0dd814aaf8443
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-07-10 05:25:04 -0300

    feat: add types vagas to register vagas

---
+++ src/types/create-vagas.ts
@@ -0,0 +1,22 @@
+import type { Modality } from "@/types/vaga"
+
+export interface RegisterVagaPayload {
+  title: string 
+  message?: string | null
+  tipo_vaga: string 
+  description: string 
+  category: string 
+  company: string 
+  requirements: string 
+  modality: Modality | null
+  salary: number | null
+  benefits: string 
+  contact: string 
+  link: string 
+  location: string  
+}
+
+export interface RegisterVagaResponse {
+    message: string
+    error: string    
+}
