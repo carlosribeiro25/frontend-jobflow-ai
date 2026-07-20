107f1e800bcf67ad2cfc574dec1e87c788485467
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-07-20 10:24:54 -0300

    refactor: reorganize footer mobile

--- src/@/components/config/footer-mobile.ts
+++ src/@/components/config/footer-mobile.ts
@@ -1,23 +1,17 @@
 import HomeFilledIcon from '@mui/icons-material/HomeFilled'
 import SettingsSuggestIcon from '@mui/icons-material/SettingsSuggest'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp'
-import FilterListAltIcon from '@mui/icons-material/FilterListAlt'
 
 export const itensNavMobile = [
   {
-    label: 'Início',
-    path: '/',
+    label: 'Home',
+    path: '/filtros',
     icon: HomeFilledIcon,
   },
   {
     label: 'Configurações',
     path: '/settings',
     icon: SettingsSuggestIcon,
-  },
-  {
-    label: 'Filtros',
-    path: '/filtros',
-    icon: FilterListAltIcon,
   },
   {
     label: 'WhatsApp',
