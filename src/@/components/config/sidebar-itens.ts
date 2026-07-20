26fc648665246bd23585aefc8b7d4a4facf78512
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-07-20 10:26:35 -0300

    refactor: reorganize sidebar itens

--- src/@/components/config/sidebar-itens.ts
+++ src/@/components/config/sidebar-itens.ts
@@ -1,5 +1,4 @@
 import HomeFilledIcon from '@mui/icons-material/HomeFilled'
-import FilterListAltIcon from '@mui/icons-material/FilterListAlt'
 import SettingsIcon from '@mui/icons-material/Settings'
 import WhatsAppIcon from '@mui/icons-material/WhatsApp'
 import UnarchiveIcon from '@mui/icons-material/Unarchive'
@@ -7,18 +6,13 @@ import UnarchiveIcon from '@mui/icons-material/Unarchive'
 export const sidebarItens = [
   {
     title: 'Home',
-    path: '/',
+    path: '/filtros',
     icon: HomeFilledIcon,
   },
   {
     title: 'Cadastro de Vagas',
     path: '/cadastrar-vagas',
     icon: UnarchiveIcon,
-  },
-  {
-    title: 'Buscar por filtros',
-    path: '/filtros',
-    icon: FilterListAltIcon,
   },
   {
     title: 'Conectar com o Whatsapp',
