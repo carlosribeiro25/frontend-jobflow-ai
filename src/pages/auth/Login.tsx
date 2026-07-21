98f3b97195aef5d169e96ef32c9ca510fb67271f
Wilson Ribeiro <contato.carlosribeiro25@gmail.com>
2026-07-21 09:32:30 -0300

    feat: add info role user authenticated loged

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -28,6 +28,7 @@ interface LoginResponse {
     name: string
     phone?: string
     picture?: string
+    role?: string
   }
 }
 
@@ -74,7 +75,10 @@ export default function Login() {
         picture: data.user.picture,
         email: data.user.email,
         phone: data.user.phone,
+        role: data.user.role
       })
+
+      console.log('dados do usuario',accessToken )
 
       Cookies.set('token', accessToken)
 
